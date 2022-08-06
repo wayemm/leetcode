62c02d393b44930a1fd863ecc14ab003caf3fefc
Echo <king@email.cn>
2022-08-06 11:22:36 +0800

    添加项目文件。

---
+++ leetcode/两数和.cpp
@@ -0,0 +1,42 @@
+//暴力解（小白思路）
+#include<iostream>
+using namespace std;
+int main() {
+	int a[10] = {1,2,3,4,5,6,7,8,9,10};
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i; j <10; j++)
+		{
+			if (a[i] + a[j] == 8)
+			{
+				cout << i << "" << j << "\t";
+			}
+		}
+	}
+	return 0;
+}
+/*#include<vector>
+#include<iostream>
+using namespace std;
+int main() {
+    Solution 对象名;
+    int a[20] = {};
+    对象名.twoSum(10);
+}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        int i, j;
+        for (i = 0; i < nums.size() - 1; i++)
+        {
+            for (j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return { i,j };
+                }
+            }
+        }
+        return { i,j };
+    };
+};*/
